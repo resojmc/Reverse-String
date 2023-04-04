14b4e55fc1f367769693665d7dda2dd33288ff13
resojmc <resojmc@gmail.com>
2023-04-04 07:30:14 -0700

    Update Reverse Str.py

--- Reverse Str.py
+++ Reverse Str.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 
+# Initializes main window and defines its geometry and title
 root = tk.Tk()
 root.geometry("300x600")
 root.title("Reverse String")
 
+# Function to reverse a given string(s)
 def reverse_str(s):
     s = str(s)
     s = s[::-1]
     return s
-    
+
+# Function updates result when button is pressed
 def update_str():
     string = top_entry.get()
     string = reverse_str(string)
@@ -26,4 +29,5 @@ button.pack()
 result_label = tk.Label(root, text="Result: ")
 result_label.pack()
 
+# Executes main loop
 root.mainloop()
